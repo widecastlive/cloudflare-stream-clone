b8c49d247dd797fbd1d3eb022d1992e1533243f5
Miran Hojnik <miran@3dot.net>
2023-10-24 11:39:06 +0200

    small updates

--- clone.sh
+++ clone.sh
@@ -2,9 +2,9 @@
 npm install -y
 node index.js $1 $2
 
-wget --content-disposition -P ./data/audio/ -i ./data/audio/segments.txt
-wget --content-disposition -P ./data/360/ -i ./data/360/segments.txt
-wget --content-disposition -P ./data/720/ -i ./data/720/segments.txt
-wget --content-disposition -P ./data/1080/ -i ./data/1080/segments.txt
+wget --retry-connrefused --waitretry=5 --read-timeout=20 --timeout=15 -t 10 --content-disposition -q --show-progress -P ./data/audio/ -i ./data/audio/segments.txt
+wget --retry-connrefused --waitretry=5 --read-timeout=20 --timeout=15 -t 10 --content-disposition -q --show-progress -P ./data/360p/ -i ./data/360p/segments.txt
+wget --retry-connrefused --waitretry=5 --read-timeout=20 --timeout=15 -t 10 --content-disposition -q --show-progress -P ./data/720p/ -i ./data/720p/segments.txt
+wget --retry-connrefused --waitretry=5 --read-timeout=20 --timeout=15 -t 10 --content-disposition -q --show-progress -P ./data/1080p/ -i ./data/1080p/segments.txt
 
 for file in ./data/*/*.mp4\?*; do mv "$file" "${file%%\?*}"; done
